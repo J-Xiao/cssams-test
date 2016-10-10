b457de0ebe79a122f7f55629d44071f7e4c50553
J-Xiao <xiaojijundavid@gmail.com>
2016-10-10 15:33:52 -0400

    Register push.

--- cssams-test/src/app/app.component.ts
+++ cssams-test/src/app/app.component.ts
@@ -19,7 +19,15 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.register();
+
       StatusBar.styleDefault();
+    });
+  }
+
+  register () {
+    this.push.register().then((token: PushToken) => {
+      return this.push.saveToken(token);
     });
   }
 }
